d88d26b7dd73ca8c5da0942a51f221bb544b7cf6
johan <johan.estrada-p@mail.escueliang.edu.co>
2022-09-09 23:29:43 -0500

    Commit

--- Funciones.py
+++ Funciones.py
@@ -8,7 +8,7 @@ def producto(num1,num2):
     """Tupla,Tupla->Tupla
     Retorna el producto de dos numeros complejos
     """
-    return (num1[0]*num2[0]-num1[1]*num2[1],num1[0]*num2[1]+num1[1]*num2[0])
+    return ( round(num1[0]*num2[0]-num1[1]*num2[1],2),round(num1[0]*num2[1]+num1[1]*num2[0],2))
 def resta(num1,num2):
     """Tupla,Tupla->Tupla
     Retorna la suma de dos numeros complejos
@@ -28,7 +28,7 @@ def modulo(num1):
     retorna el modulo de un numero complejo
     """
     return sqrt(num1[0]**2+num1[1]**2)
-def conjugado(num1):
+def conjugado_v(num1):
     """
     Tupla->Real
     returna el conjugado del numero complejo
@@ -61,35 +61,62 @@ def fase(coordenadas, tipo):
     else:
         return coordenadas[1]
 def ad_vec_complex(vec1,vec2):
+    """
+    Vec_complex,Vec_complex->Vec_complex
+    Retorna la suma de dos vectores complejos
+    """
     new_vec=[0 for i in range(len(vec1))]
     for i in range(len(new_vec)):
         new_vec[i]=suma(vec1[i],vec2[i])
     return new_vec
 def inverse_add(vec):
+    """
+    Vec_complex->Vec_complex
+    Retorna el inverso aditivo de un vector
+    """
     inverse=[]
     for i in range(len(vec)):
         inverse.append((-vec[i][0],-vec[i][1]))
     return inverse
 def esc_complex(esc,vec):
+    """
+    complex,matriz->matriz
+    Retorna el producto de un complejo por cada elemento de una matriz
+    """
     for i in range(len(vec)):
         vec[i]=producto(vec[i],esc)
     return vec
 def add_mat_complex(mat1,mat2):
+    """
+    matriz,matriz->matriz
+    retorna la suma de dos matrices complejas
+    """
     for i in range(len(mat1)):
         for j in range(len(mat1[0])):
             mat1[i][j]=suma(mat1[i][j],mat2[i][j])
     return mat1
 def inverse_mat(mat):
+    """
+    matriz->matriz
+    retorna el inverso aditivo de una matriz
+    """
     for i in range(len(mat)):
         mat[i]=inverse_add(mat[i])
     return mat
 def esc_mat(esc,mat):
+    """
+    comlex,matriz->matriz
+    retorna la multiplicacion de una matriz con un numero complejo
+    """
     for i in range(len(mat)):
         for j in range(len(mat[0])):
             mat[i][j]=producto(esc,mat[i][j])
     return mat
 def traspuesta(mat):
-    elements=[]
+    """
+    mat->mat
+    retorna la traspuesta de una matriz
+    """
     if type(mat[0])==int:
         new_mat = [[] for i in range(len(mat))]
         for i in range(len(mat)):
@@ -101,6 +128,10 @@ def traspuesta(mat):
                 new_mat[i].append(mat[j][i])
     return new_mat
 def conjugada(mat):
+    """
+    matriz->matriz
+    retorna la conjugada de una matriz
+    """
     if type(mat[0])==tuple:
         for i in range(len(mat)):
             mat[i]=(mat[i][0],-mat[i][1])
@@ -110,6 +141,10 @@ def conjugada(mat):
                 mat[i][j]=(mat[i][j][0],-mat[i][j][1])
     return mat
 def adjunta(mat):
+    """
+    matriz->matriz
+    retorna la adjunta de una matriz
+    """
     return(conjugada(traspuesta(mat)))
 def prod_mat(mat1,mat2):
     n=len(mat1)
@@ -148,48 +183,130 @@ def distancia(vec1,vec2):
         vec2[i]=-vec2[i][0],-vec2[i][1]
         new_vec.append(suma(vec1[i],vec2[i]))
     return norma(new_vec)
-print(distancia([(0,0),(0,0),(0,0),(0,0)],[(4,3),(6,-4),(12,-7),(0,13)]))
+def mat_unitaria(mat):
+    new_mat=[[(0,0) for j in range(len(mat[0]))] for i in range(len(mat))]
+    for i in range(len(new_mat)):
+        for j in range(len(new_mat[0])):
+            if i==j:
+                new_mat[i][j]=(1,0)
+    mat2=mat[:]
+    unit=prod_mat(mat,mat2)
+    cond=True
+    for i in range(len(unit)):
+        for j in range(len(unit[i])):
+            cond=(unit[i][j][0]==new_mat[i][j][0] and unit[i][j][1]==new_mat[i][j][1])
+            if not cond:
+                return False
+    return cond
+def hermit(mat):
+    return adjunta(mat[:])==mat
+def prod_tensor(element1,element2):
+    filas=len(element1*len(element2))
+    if type(element1[0])==tuple or type(element2[0])==tuple:
+        columnas=1
+    else:
+        columnas=len(element1[0])*len(element2[0])
+    new_mat=[]
+    for i in range(len(element1)):
+        for j in range(len(element1[0])):
+            element1[i][j]=esc_mat(element1[i][j],element2)
+    for i in element1:
+        print(i)
+
+print(prod_tensor([[(1,0),(2,3)],[(1,0),(1,1)]],[[(1,0),(2,3)],[(1,0),(1,1)]]))
+
+
 
 
 
 ##Pruebas:
-#print("Pruebas suma:")
-#print("1:")
-#print(suma((1,4),(8,-9)))
-#print("2:")
-#print(suma((7,-1),(8,-2)))
-#print("Pruebas resta:")
-#print("1:")
-#print(resta((1,4),(-8,-9)))
-#print("2:")
-#print(resta((7,-1),(8,-2)))
-#print("Pruebas producto:")
-#print("1:")
-#print(producto((1,5),(8,0)))
-#print("2:")
-#print(producto((7,-1),(4,-2)))
-#print("Pruebas division:")
-#print("1:")
-#print(division((1,4),(-8,-9)))
-#print("2:")
-#print(division((7,-1),(8,-2)))
-#print("Pruebas modulo")
-#print("1:")
-#print(modulo((1,4)))
-#print("2:")
-#print(modulo((7,-1)))
-#print("Pruebas conjugado")
-#print("1:")
-#print(conjugado((1,4)))
-#print("2:")
-#print(conjugado((8,-2)))
-#print("Pruebas cambio de coordenadas")
-#print("1:")
-#print(pol_cart((1,5),"cartesianas"))
-#print("2:")
-#print(pol_cart((2,45),"polares"))
-#print("Pruebas fase")
-#print("1:")
-#print(fase((1,4),"cartesianas"))
-#print("2:")
-#print(fase((7,-1),"polares"))
+print("Pruebas suma:")
+print("1:")
+print(suma((1,4),(8,-9)))
+print("2:")
+print(suma((7,-1),(8,-2)))
+print("Pruebas resta:")
+print("1:")
+print(resta((1,4),(-8,-9)))
+print("2:")
+print(resta((7,-1),(8,-2)))
+print("Pruebas producto:")
+print("1:")
+print(producto((1,5),(8,0)))
+print("2:")
+print(producto((7,-1),(4,-2)))
+print("Pruebas division:")
+print("1:")
+print(division((1,4),(-8,-9)))
+print("2:")
+print(division((7,-1),(8,-2)))
+print("Pruebas modulo")
+print("1:")
+print(modulo((1,4)))
+print("2:")
+print(modulo((7,-1)))
+print("Pruebas conjugado")
+print("1:")
+print(conjugado_v((1,4)))
+print("2:")
+print(conjugado_v((8,-2)))
+print("Pruebas cambio de coordenadas")
+print("1:")
+print(pol_cart((1,5),"cartesianas"))
+print("2:")
+print(pol_cart((2,45),"polares"))
+print("Pruebas fase")
+print("1:")
+print(fase((1,4),"cartesianas"))
+print("2:")
+print(fase((7,-1),"polares"))
+print("Pruebas Adición de vectores complejos")
+print("1:")
+print(ad_vec_complex([(1,0),(1,1),(-4,5)],[(1,2),(4,4),(7,1)]))
+print("2:")
+print(ad_vec_complex([(1,5),(1,1)],[(4,5),(2,0)]))
+print("Pruebas Inverso (aditivo) de un vector complejo.")
+print("1:")
+print(inverse_add([(1,0),(1,1),(-4,5)]))
+print("2:")
+print(inverse_add([(1,5),(1,1)]))
+print("Pruebas Multiplicación de un escalar por un vector complejo.")
+print("1:")
+print(esc_mat((4,1),[[(1,0),(2,4)],[(5,4),(1,1)]]))
+print("2:")
+print(esc_mat((0,1),[[(1,1),(1,0),(2,4)],[(8,8),(5,4),(1,1)]]))
+print("Pruebas Adición de matrices complejas.")
+print("1:")
+print(add_mat_complex([[(1,2),(2,3)],[(4,5),(1,1)],[(1,0),(4,1)]],[[(1,1),(0,0)],[(1,8),(10,9)],[(1,0),(3,4)]]))
+print("2:")
+print(add_mat_complex([[(1,8),(0,3),(4,4)],[(0,1),(0,0),(-100,9)],[(1,0),(4,1),(0,9)]],[[(1,1),(10,10),(7,6)],[(2,2),(1,8),(10,9)],[(4,4),(1,0),(3,4)]]))
+print("Pruebas Inversa (aditiva) de una matriz compleja.")
+print("1:")
+print(inverse_mat([[(1,8),(0,3),(4,4)],[(0,1),(0,0),(-100,9)],[(1,0),(4,1),(0,9)]]))
+print("2:")
+print(inverse_mat([[(1,1),(0,0)],[(1,8),(10,9)],[(1,0),(3,4)]]))
+print("Pruebas Multiplicación de un escalar por una matriz compleja.")
+print("1:")
+print(esc_mat((0,1),[[(1,8),(0,3),(4,4)],[(0,1),(0,0),(-100,9)],[(1,0),(4,1),(0,9)]]))
+print("2:")
+print(esc_mat((1,8),[[(1,0),(2,4)],[(5,4),(1,1)]]))
+print("Pruebas Transpuesta de una matriz/vector")
+print("1:")
+print(traspuesta([[(1,0),(2,4)],[(5,4),(1,1)]]))
+print("2")
+print([(1,0),(1,1),(-4,5)])
+print("Pruebas Conjugada de una matriz/vector")
+print("1:")
+print(conjugada([[(1,0),(2,4)],[(5,4),(1,1)]]))
+print("2:")
+print(conjugada([(1,1),(0,0)]))
+print("Pruebas adjunta")
+print("1:")
+print(adjunta([[(1,8),(0,3),(4,4)],[(0,1),(0,0),(-100,9)],[(1,0),(4,1),(0,9)]]))
+print("2:")
+print(adjunta([(1,0),(2,4)]))
+print("Pruebas producto matrices:")
+print("1:")
+print(prod_mat([[(1,8),(0,3),(4,4)],[(0,1),(0,0),(-100,9)],[(1,0),(4,1),(0,9)]],[[(14,-8),(30,13),(14,40)],[(0,1),(0,0),(-100,90)],[(1,-10),(4,0),(1,1)]]))
+print("2:")
+print(prod_mat([[(1,8),(0,3),(4,4)],[(0,1),(0,0),(-100,9)],[(1,0),(4,1),(0,9)]],[[(1,8),(0,3),(4,4)],[(10,10),(0,0),(21,9)],[(1,0),(14,1),(7,9)]]))
--- Readme.md
+++ Readme.md
@@ -9,6 +9,23 @@
 - Conjugado
 - Conversión entre coordenadas polares y cartesianas
 - Obtener la fase de un número complejo
+- Adición de vectores complejos.
+- Inverso (aditivo) de un vector complejo.
+- Multiplicación de un escalar por un vector complejo.
+- Adición de matrices complejas.
+- Inversa (aditiva) de una matriz compleja.
+- Multiplicación de un escalar por una matriz compleja.
+- Transpuesta de una matriz/vector
+- Conjugada de una matriz/vector
+- Adjunta (daga) de una matriz/vector
+- Producto de dos matrices (de tamaños compatibles)
+- Función para calcular la "acción" de una matriz sobre un vector.
+- Producto interno de dos vectores
+- Norma de un vector
+- Distancia entre dos vectores
+- Revisar si una matriz es unitaria
+- Revisar si una matriz es Hermitiana
+- Producto tensor de dos matrices/vectores
 #
 ### Elaborado con:
 - [PyCharm](https://www.jetbrains.com/es-es/pycharm/)
